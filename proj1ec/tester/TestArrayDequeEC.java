8e05fbd51b2b2a6e1032a4384ed46713836c384d
m-contour <m-contour@qq.com>
2023-04-19 10:22:39 +0800

    proj1-ec: Finish this bonus part

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,40 @@
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import student.StudentArrayDeque;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class TestArrayDequeEC {
+    @Test
+    public void randomArrayDequeTest() {
+        StudentArrayDeque<Integer> stad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stad2 = new ArrayDequeSolution<>();
+
+        int M = 5000;
+        StringBuilder str = new StringBuilder("\n");
+        for (int i = 0; i < M; i++) {
+            int operationNumber = StdRandom.uniform(0, 6);
+            if (operationNumber == 0) {
+                int randVal= StdRandom.uniform(0, 100);
+                stad1.addFirst(randVal);
+                stad2.addFirst(randVal);
+                str.append("AddFirst(").append(randVal).append(")\n");
+            } else if (operationNumber == 1) {
+                int randVal= StdRandom.uniform(0, 100);
+                stad1.addLast(randVal);
+                stad2.addLast(randVal);
+                str.append("AddLast(").append(randVal).append(")\n");
+            } else if (stad1.size() == 0) {
+                assertTrue(stad1.isEmpty());
+            } else if (operationNumber == 2) {
+                str.append("removeFirst()\n");
+                assertEquals(String.valueOf(str), stad1.removeFirst(), stad2.removeFirst());
+            } else if (operationNumber == 3) {
+                str.append("removeLast()\n");
+                assertEquals(String.valueOf(str), stad1.removeLast(), stad2.removeLast());
+            }
+        }
+
+    }
+}
